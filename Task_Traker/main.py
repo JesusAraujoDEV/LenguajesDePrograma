907ffd2cf458a1721cc704af8498d658f4081097
Sperrotta10 <santiagoperrotta10@gmail.com>
2025-02-03 16:01:16 -0400

    estructura del main para controlar los comandos de la terminal

--- Task_Traker/main.py
+++ Task_Traker/main.py
@@ -1,7 +1,7 @@
 import sys
 import datetime
 import json
-import Class_task
+from Class_task import Utilidad_json
 
 
 def mostrar_ayuda():
@@ -24,12 +24,48 @@ if __name__ == '__main__':
     print("\t ----------- Task Traker -----------")
     print("\t ===================================")
 
-    print("\nBienvenido al task traker, <utiliza el comando 'help' para mostrar la lista de comandos>")
+    print("\nBienvenido al task traker, <utiliza el comando 'help' para mostrar la lista de comandos>\n")
 
+    # si tiene mas de un argumento accede a las condiciones
     if(len(sys.argv) > 1):
 
+        # lista de comandos
         if(sys.argv[1].lower() == 'help'):
             mostrar_ayuda()
+        
+        # verificamos si el argumento es 'create'
+        elif(sys.argv[1].lower() == 'create'):
+
+            # creamos una tarea
+            Utilidad_json().create_task()
+
+        # verificamos si el argumento es 'modify-tittle'
+        elif(sys.argv[1].lower() == 'modify-tittle'):
+            pass
+
+        # verificamos si el argumento es 'modify-status'
+        elif(sys.argv[1].lower() == 'modify-status'):
+            pass
+
+        # verificamos si el argumento es 'modify-expiration_date'
+        elif(sys.argv[1].lower() == 'modify-expiration_date'):
+            pass
+
+        # verificamos si el argumento es 'list-all'
+        elif(sys.argv[1].lower() == 'list-all'):
+            pass
+
+        # verificamos si el argumento es 'list-status'
+        elif(sys.argv[1].lower() == 'list-status'):
+            pass
+
+        # verificamos si el argumento es 'list-expiration_date'
+        elif(sys.argv[1].lower() == 'list-expiration_date'):
+            pass
+
+        # verificamos si el argumento es 'delete'
+        elif(sys.argv[1].lower() == 'delete'):
+            pass
 
 
 
