c632d7f17ed5c139dc8390c73265a58d1af2bed7
Sperrotta10 <santiagoperrotta10@gmail.com>
2025-02-03 15:06:28 -0400

    Folder Creation for the task tracker, program structure, class_task, readme

---
+++ Task_Traker/Class_task.py
@@ -0,0 +1,73 @@
+import sys
+import datetime
+import json
+
+# Clase para crear una tarea del task traker
+class Task:
+
+    def __init__(self,titulo,descripcion,fecha_vencimiento):
+        self.titulo = titulo
+        self.descripcion = descripcion
+        self.status = "Pendiente"
+        self.fecha_creacion = datetime.datetime().now().strftime("%Y-%m-%d %H:%M:%S")
+        self.fecha_vencimiento = fecha_vencimiento
+
+    # Metodo para crear una tarea
+    def create_task(self):
+        pass
+
+
+
+# Clase de utilidad para manejar el json donde se guardan las tareas del task traker
+class Utilidad_json():
+
+    def __init__(self):
+        pass
+
+    def read_tasks(self,operation):
+
+        # listar alls task, for status (Pending, progress, Completed), for expiration date
+
+        if(operation == "Pendiente"):
+            pass
+
+        elif(operation == "Progreso"):
+            pass
+        
+        elif(operation == "Completada"):
+            pass
+
+        elif(operation == "Vencimiento"):
+            pass
+
+        else:
+            pass
+        
+        pass
+
+    def write_task(self):
+        pass
+
+    def modify_task(self, operation):
+
+        # modify status, tittle, description, expiration date 
+
+        if(operation == "Titulo"):
+            pass
+
+        elif(operation == "descripcion"):
+            pass
+        
+        elif(operation == "Vencimiento"):
+            pass
+
+        elif(operation == "status"):
+            pass
+
+        pass
+
+    def delete_task(self):
+        pass
+
+
+
---
+++ Task_Traker/main.py
@@ -0,0 +1,13 @@
+import sys
+import datetime
+import json
+import Class_task
+
+
+def main():
+
+    print("===================================")
+    print("----------- Task Traker -----------")
+    print("===================================")
+
+main()
