15645f9acddf4cb989971fdb10f4ce6aab1c6435
Sperrotta10 <santiagoperrotta10@gmail.com>
2025-02-03 15:43:31 -0400

    funcion para mostrar una lista de comandos, interfaz en la terminal

--- Task_Traker/main.py
+++ Task_Traker/main.py
@@ -4,10 +4,34 @@ import json
 import Class_task
 
 
-def main():
+def mostrar_ayuda():
 
-    print("===================================")
-    print("----------- Task Traker -----------")
-    print("===================================")
+    print("""
+        - 'create' <crear una tarea>
+        - 'modify-tittle' <modificar el titulo de una tarea>
+        - 'modify-status' <modificar el status de una tarea>
+        - 'modify-expiration_date' <modificar el vencimiento de una tarea>
+        - 'list-all' <listar todas las tareas>
+        - 'list-status' <listar por status>
+        - 'list-expiration_date' <listar por fecha de vencimiento>
+        - 'delete' <eliminar una tarea>
+    """)
 
-main()
+
+if __name__ == '__main__':
+
+    print("\t ===================================")
+    print("\t ----------- Task Traker -----------")
+    print("\t ===================================")
+
+    print("\nBienvenido al task traker, <utiliza el comando 'help' para mostrar la lista de comandos>")
+
+    if(len(sys.argv) > 1):
+
+        if(sys.argv[1].lower() == 'help'):
+            mostrar_ayuda()
+
+
+
+
+
