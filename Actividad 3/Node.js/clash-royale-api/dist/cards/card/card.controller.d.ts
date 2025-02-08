3563f88ba50452472d72668d4cd2949cf88ca28b
alemt19 <141878706+alemt19@users.noreply.github.com>
2025-02-07 22:31:58 -0400

    Delete Actividad 3/Node.js/clash-royale-api/dist directory

--- Actividad 3/Node.js/clash-royale-api/dist/cards/card/card.controller.d.ts
+++
@@ -1,9 +0,0 @@
-import { CardService } from './card.service';
-import { Card } from './card.entity';
-import { Request } from 'express';
-export declare class CardController {
-    private readonly cardService;
-    constructor(cardService: CardService);
-    findAll(): Promise<Card[]>;
-    create(request: Request): Promise<Card>;
-}
