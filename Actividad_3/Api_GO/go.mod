839e5a34c5b0bbd17901420e1433a47d2a0f3c6b
Maanuuuu <manueldavidnava31@gmail.com>
2025-02-10 15:33:07 -0400

    Listo los metodos de GET,POST,DELETE de la api

--- Actividad_3/Api_GO/go.mod
+++ Actividad_3/Api_GO/go.mod
@@ -1,3 +1,10 @@
 module github.com/JesusAraujoDEV/LenguajesDePrograma
 
 go 1.23.5
+
+require (
+
+	github.com/githubnemo/CompileDaemon v1.4.0 // indirect
+	github.com/gorilla/mux v1.8.1 // indirect
+
+)
--- Actividad_3/Api_GO/main.go
+++ Actividad_3/Api_GO/main.go
@@ -1,7 +1,98 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"strconv"
+
+	"github.com/gorilla/mux"
+)
+
+type task struct {
+	ID      int    `json:ID`
+	Name    string `json:Name`
+	Content string `json:Content`
+}
+
+type AllTasks []task
+
+var tasks = AllTasks{
+	{
+		ID:      1,
+		Name:    "Tarea 1",
+		Content: "Some Content",
+	},
+}
+
+func createTasks(w http.ResponseWriter, r *http.Request) {
+	var newTask task
+	reqBody, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		fmt.Fprint(w, "Insert a valid Task")
+	}
+	json.Unmarshal(reqBody, &newTask)
+	newTask.ID = len(tasks) + 1
+	tasks = append(tasks, newTask)
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+	json.NewEncoder(w).Encode(newTask)
+}
+
+func getTasks(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(tasks)
+}
+
+func getTask(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	vars := mux.Vars(r)
+	task_id, err := strconv.Atoi(vars["id"])
+	if err != nil {
+		fmt.Fprint(w, " Invalid ID")
+		return
+	}
+
+	for _, task := range tasks {
+		if task.ID == task_id {
+			json.NewEncoder(w).Encode(task)
+		}
+	}
+}
+
+func DeleteTask(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	task_id, err := strconv.Atoi(vars["id"])
+	if err != nil {
+		fmt.Fprint(w, " Invalid ID")
+		return
+	}
+
+	for i, task := range tasks {
+		if task.ID == task_id {
+			tasks = append(tasks[:i], tasks[i+1:]...)
+			fmt.Fprint(w, "The task with ID %v has been removed succesfully", task_id)
+		}
+	}
+
+}
+
+func indexRoute(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "WELCOME TO ADO API LETSGOOOOO")
+}
 
 func main() {
-	fmt.Print("Hola GO")
+
+	router := mux.NewRouter().StrictSlash(true)
+
+	router.HandleFunc("/", indexRoute)
+	router.HandleFunc("/tasks", getTasks).Methods("GET")
+	router.HandleFunc("/tasks", createTasks).Methods("POST")
+	router.HandleFunc("/tasks/{id}", getTask).Methods("GET")
+	router.HandleFunc("/tasks/{id}", DeleteTask).Methods("DELETE")
+
+	log.Fatal(http.ListenAndServe(":3000", router))
 }
