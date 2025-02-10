507b88957004e65945c22908083b7e2fd9922f93
JesusAraujoDEV <j2a0a0a5@gmail.com>
2025-02-10 17:36:11 -0400

    Endpoint Post Customer

---
+++ Actividad 3/FastAPI/app/routers/customers.py
@@ -0,0 +1,14 @@
+from fastapi import APIRouter, HTTPException, status
+from sqlmodel import select
+from models import Customer, CustomerCreate, CustomerUpdate
+from db import SessionDep
+
+router = APIRouter()
+
+@router.post("/customers", response_model=Customer, tags=["Customers"])
+async def create_customer(customer_data: CustomerCreate, session: SessionDep):
+    customer = Customer.model_validate(customer_data.model_dump())
+    session.add(customer)
+    session.commit()
+    session.refresh(customer)
+    return customer
